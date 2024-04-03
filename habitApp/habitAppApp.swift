84e467f0f40e56d356fe5b1492c6a08e596d5adb
keikeikei0001 <keidm0802@gamail.com>
2024-04-03 23:06:00 +0900

    Initial Commit

---
+++ habitApp/habitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitAppApp.swift
+//  habitApp
+//
+//  Created by 松田圭右 on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct habitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
