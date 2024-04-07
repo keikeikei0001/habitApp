a9efbfd608d65ab83af5f3bc9833c55246f83929
keikeikei0001 <keidm0802@gamail.com>
2024-04-07 22:33:44 +0900

    2024.04.07 TaskAddView.swift タスク追加ボタン押下時の処理をfunction化。

--- habitApp/View/TaskAddView.swift
+++ habitApp/View/TaskAddView.swift
@@ -13,9 +13,11 @@ struct TaskAddView: View {
     @Binding var isTaskAddView: Bool
     
     var body: some View {
-        Spacer()
-        TaskAddMain(isTaskAddView: $isTaskAddView)
-        Spacer()
+        VStack {
+            Spacer()
+            TaskAddMain(isTaskAddView: $isTaskAddView)
+            Spacer()
+        }
     }
 }
 
@@ -33,27 +35,36 @@ struct TaskAddMain: View {
     let taskManager = TaskManager()
     
     var body: some View {
-        //画面.タスク入力テキストフィールド
-        TextField("タスク名を入力してください", text: $inputTaskName)
-            .textFieldStyle(.roundedBorder)
-            .padding()
-            .focused($isFocused)
-        Spacer()
-        Button {
-            isFocused = false
-            //タスク情報取得
-            var todoArray = taskManager.loadTask(forKey: "taskData") ?? []
-            //新しいタスクを追加し、保存
-            let taskData = TaskData(taskName: inputTaskName)
-            todoArray.append(taskData)
-            taskManager.saveTask(taskArray: todoArray, forKey: "taskData")
-            //追加されたタスク情報を画面に描画
-            taskObject.taskData = taskManager.loadTask(forKey: "taskData") ?? []
-            //モーダル遷移を閉じる
-            isTaskAddView = false
-        }label: {
-            Text("Create")
-                .modifier(CustomModifier(color: .blue))
+        VStack {
+            //画面.タスク入力テキストフィールド
+            TextField("タスク名を入力してください", text: $inputTaskName)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .focused($isFocused)
+            Spacer()
+            Button {
+                //タスク追加ボタン押下時メソッド
+                taskAdd()
+            }label: {
+                Text("Create")
+                    .modifier(CustomModifier(color: .blue))
+            }
         }
+    }
+    
+    //タスク追加ボタン押下時
+    func taskAdd() {
+        //テキストフィールドへのフォーカス解除
+        isFocused = false
+        //タスク情報取得
+        var todoArray = taskManager.loadTask(forKey: "taskData") ?? []
+        //新しいタスクを追加し、保存
+        let taskData = TaskData(taskName: inputTaskName)
+        todoArray.append(taskData)
+        taskManager.saveTask(taskArray: todoArray, forKey: "taskData")
+        //追加されたタスク情報を画面に描画
+        taskObject.taskData = taskManager.loadTask(forKey: "taskData") ?? []
+        //モーダル遷移を閉じる
+        isTaskAddView = false
     }
 }
