7af28e08f5cf9b91eebe4b445ee4cc8f59a4677f
keikeikei0001 <keidm0802@gamail.com>
2024-04-07 22:08:20 +0900

    DateUtil.swift 追加

---
+++ habitApp/Util/DateUtil.swift
@@ -0,0 +1,45 @@
+//
+//  Data.swift
+//  habit
+//
+//  Created by 松田圭右 on 2024/02/12.
+//
+
+import Foundation
+
+extension TimeZone {
+    static let japan = TimeZone(identifier: "Asia/Tokyo")!
+}
+
+extension Locale {
+    static let japan = Locale(identifier: "ja_JP")
+}
+
+extension Date {
+    //日本時間に修正
+    var zeroclock: Date {
+        return fixed(hour: 9, minute: 0, second: 0)
+    }
+    
+    var calendar: Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .japan
+        calendar.locale   = .japan
+        return calendar
+    }
+    
+    func fixed(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        let calendar = self.calendar
+        
+        var comp = DateComponents()
+        comp.year   = year   ?? calendar.component(.year,   from: self)
+        comp.month  = month  ?? calendar.component(.month,  from: self)
+        comp.day    = day    ?? calendar.component(.day,    from: self)
+        comp.hour   = hour   ?? calendar.component(.hour,   from: self)
+        comp.minute = minute ?? calendar.component(.minute, from: self)
+        comp.second = second ?? calendar.component(.second, from: self)
+        
+        return calendar.date(from: comp)!
+    }
+}
+
