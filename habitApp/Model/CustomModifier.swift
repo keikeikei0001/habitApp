e8c089d67a0d1dcc6e09993d6af05ca5e26d9d77
keikeikei0001 <keidm0802@gamail.com>
2024-04-08 23:17:24 +0900

    2024.04.08 CustomModifier.swift コメントアウト

--- habitApp/Model/CustomModifier.swift
+++ habitApp/Model/CustomModifier.swift
@@ -4,19 +4,19 @@
 //
 //  Created by 松田圭右 on 2024/04/07.
 //
-
-import SwiftUI
-
-//カスタムモディファイア
-struct CustomModifier: ViewModifier {
-    let color: Color
-    func body(content: Content) -> some View {
-        content
-            .padding(.horizontal, 50)
-            .padding(.vertical)
-            .background(color)
-            .foregroundColor(.white)
-            .font(.system(size: 25, design: .default))
-            .cornerRadius(10)
-    }
-}
+//
+//import SwiftUI
+//
+////カスタムモディファイア
+//struct CustomModifier: ViewModifier {
+//    let color: Color
+//    func body(content: Content) -> some View {
+//        content
+//            .padding(.horizontal, 50)
+//            .padding(.vertical)
+//            .background(color)
+//            .foregroundColor(.white)
+//            .font(.system(size: 25, design: .default))
+//            .cornerRadius(10)
+//    }
+//}
