f491598fade04de424a64a8d43ceb27da3339405
RikutoSato <satorikuto1064@gmail.com>
2024-10-18 23:28:56 +0900

    AppDelegateをファイルわけ

---
+++ habitApp/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  habitApp
+//
+//  Created by RikutoSato on 2024/10/18.
+//
+
+import FirebaseCore
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
--- habitApp/App/habitAppApp.swift
+++ habitApp/App/habitAppApp.swift
@@ -6,20 +6,9 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        return true
-    }
-}
 
 @main
 struct habitAppApp: App {
-    // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
@@ -30,5 +19,3 @@ struct habitAppApp: App {
         }
     }
 }
-
-
